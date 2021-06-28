6352b2135ebe57279dce01e9669126a8f22f2ec7
ThomasDravigney <56030910+ThomasDravigney@users.noreply.github.com>
2021-06-29 00:34:43 +0200

    global update

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Google My Business API methods : https://developers.google.com/my-business/reference/rest?hl=fr
 # Google My Business, location metrics : https://developers.google.com/my-business/reference/rest/v4/Metric?hl=fr
 
-
+import pandas as pd
 from functions import get_location_metrics, save_local_data, get_local_data, clear_local_data, create_dataframe, timer
 
 
-metrics = ["QUERIES_DIRECT", "QUERIES_INDIRECT"]
-start_date = '2021-06-17'
-end_date = '2021-06-18'
+metrics = ["ALL"]
+start_date = '2021-06-15'
+end_date = '2021-06-21'
 
 
 @timer
@@ -18,12 +18,14 @@ def main():
     try:
         data = get_local_data()
         df = create_dataframe(data)
+
         print(df)
+
+        df.to_csv(r'C:\Users\dravi\Downloads\GMB_STATISTICS_{0}_{1}.csv'.format(start_date, end_date), index=False)
+
     except (FileNotFoundError, UnboundLocalError):
         save_local_data(get_location_metrics(metrics, start_date, end_date))
         print('local_data file has been created. Rerun to get expected result.')
-
-    # df.to_csv(r'C:\Users\dravi\Downloads\data gmb.csv', index=False)
 
 
 if __name__ == '__main__':
